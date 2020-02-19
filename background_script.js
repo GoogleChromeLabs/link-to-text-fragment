3c566c937ed6f46e221d5d11549f2bde2944c2d8
tomayac <steiner.thomas@gmail.com>
2020-02-19 16:24:55 +0100

    Some less bugs

--- background_script.js
+++ background_script.js
@@ -1,4 +1,4 @@
-((browser) => {
+(browser => {
   if (!('fragmentDirective' in window.location)) {
     return;
   }
@@ -6,7 +6,7 @@
   browser.contextMenus.create({
     title: chrome.i18n.getMessage('copy_link'),
     id: 'copy-link',
-    contexts: ['all'],
+    contexts: ['all']
   });
 
   browser.contextMenus.onClicked.addListener(async (info, tab) => {
@@ -19,35 +19,79 @@
   });
 
   const isUniqueMatch = (hayStack, textStart, textEnd = '') => {
-    const needle = textEnd ?
-        new RegExp(`\\b${textStart}\\b.*?\\b${textEnd}\\b`, 'gimus') :
-        new RegExp(`\\b${textStart}\\b`, 'giu');
-    return matches = [...hayStack.matchAll(needle)];
+    const needle = textEnd
+      ? new RegExp(`\\b${textStart}.*?${textEnd}\\b`, 'gimus')
+      : new RegExp(`\\b${textStart}\\b`, 'giu');
+    console.log('————————————————');
+    console.log(needle.source);
+    console.log([...hayStack.matchAll(needle)]);
+    console.log('————————————————');
+    return (matches = [...hayStack.matchAll(needle)]);
   };
 
-  const findUniqueMatch = (pageText, textStart, textEnd, matches, wordsBefore,
-        wordsAfter, before = '', after = '') => {
+  const findUniqueMatch = (
+    pageText,
+    textStart,
+    textEnd,
+    matches,
+    wordsBefore,
+    wordsAfter,
+    before = '',
+    after = ''
+  ) => {
+    console.log('start "' + textStart + '"');
+    console.log('end "' + textEnd + '"');
+    console.log('wordsbefore "' + wordsBefore.join(',') + '"');
+    console.log('wordsafter "' + wordsAfter.join(',') + '"');
+    console.log('before "' + before + '"');
+    console.log('after "' + after + '"');
     // We need to add context before or after the needle.
     if (wordsBefore.length > 0) {
+      console.log('before pop');
+      console.log(wordsBefore);
       const newBefore = wordsBefore.pop();
+      console.log('after pop');
+      console.log(wordsBefore);
+      console.log('new before "' + newBefore + '"');
       before = `${newBefore}${before ? ` ${before}` : ''}`;
-      matches = isUniqueMatch(pageText, `${before} ${textStart}`, textEnd);
     } else if (wordsAfter.length > 0) {
+      console.log('before shift');
+      console.log(wordsAfter);
       const newAfter = wordsAfter.shift();
-      after = `${after ? `${after} ` : ''}${newAfter}`;
-      matches = isUniqueMatch(pageText, textStart, `${textEnd} ${after}`);
+      console.log('after shift');
+      console.log(wordsAfter);
+      console.log('new after "' + newAfter + '"');
+      after = `${after ? `${after}.?` : ''}${newAfter}`;
     }
+    matches = isUniqueMatch(
+      pageText,
+      `${before}.?${textStart}`,
+      `${textEnd}.?${after}`
+    );
     if (matches.length === 1) {
       return {
         before,
-        after,
-      }
+        after
+      };
+    } else if (matches.length === 0) {
+      return {
+        before: false,
+        after: false
+      };
     }
-    return findUniqueMatch(pageText, textStart, textEnd, matches, wordsBefore,
-        wordsAfter, before, after);
+    return findUniqueMatch(
+      pageText,
+      textStart,
+      textEnd,
+      matches,
+      wordsBefore,
+      wordsAfter,
+      before,
+      after
+    );
   };
 
-  const encodeURIComponentAndMinus = (text) => {
+  const encodeURIComponentAndMinus = text => {
     return encodeURIComponent(text).replace(/-/g, '%2D');
   };
 
@@ -58,8 +102,14 @@
       pageText,
       textBeforeSelection,
       textAfterSelection,
+      textNodeBeforeSelection,
+      textNodeAfterSelection
     } = await getPageTextContent();
-    const selectedWords = selectedText.split(/\s/gmu);
+    console.log(textNodeBeforeSelection);
+    console.log(textBeforeSelection);
+    console.log(textAfterSelection);
+    console.log(textNodeAfterSelection);
+    const selectedWords = selectedText.split(/\s+/gmu);
     let textStart = selectedText;
     let textEnd = '';
     if (selectedWords.length > 1) {
@@ -71,40 +121,78 @@
     if (matches.length === 1) {
       textStart = encodeURIComponentAndMinus(textStart);
       textEnd = textEnd ? `,${encodeURIComponentAndMinus(textEnd)}` : '';
-      return textFragmentURL += `#:~:text=${textStart}${textEnd}`;
+      return (textFragmentURL += `#:~:text=${textStart}${textEnd}`);
     } else {
-      const wordsBefore = textBeforeSelection.split(/\s+/gm);
-      const wordsAfter = textAfterSelection.split(/\s+/gm);
-      let {before, after} = findUniqueMatch(pageText, textStart, textEnd,
-          matches, wordsBefore, wordsAfter);
+      let wordsBefore = (textNodeBeforeSelection
+        ? textNodeBeforeSelection.split(/\s+/gmu)
+        : []
+      ).concat(textBeforeSelection ? textBeforeSelection.split(/\s+/gmu) : []);
+      let wordsAfter = (textAfterSelection
+        ? textAfterSelection.split(/\s+/gmu)
+        : []
+      ).concat(
+        textNodeAfterSelection ? textNodeAfterSelection.split(/\s+/gmu) : []
+      );
+      let { before, after } = findUniqueMatch(
+        pageText,
+        textStart,
+        textEnd,
+        matches,
+        wordsBefore,
+        wordsAfter
+      );
+      if (!before && !after) {
+        return await sendMessageToPage('failure');
+      }
       before = before ? `${encodeURIComponentAndMinus(before)}-,` : '';
       after = after ? `,-${encodeURIComponentAndMinus(after)}` : '';
       textStart = encodeURIComponentAndMinus(textStart);
       textEnd = textEnd ? `,${encodeURIComponentAndMinus(textEnd)}` : '';
-      return textFragmentURL +=
-          `#:~:text=${before}${textStart}${textEnd}${after}`;
+      return (textFragmentURL += `#:~:text=${before}${textStart}${textEnd}${after}`);
     }
   };
 
   const getPageTextContent = async () => {
-    return new Promise((resolve) => {
-      browser.tabs.query({
-        active: true,
-        currentWindow: true
-      }, (tabs) => {
-        browser.tabs.sendMessage(tabs[0].id, {
-          message: 'get-text',
-        }, (response) => {
-          return resolve(response);
-        });
-      });
+    return new Promise(resolve => {
+      browser.tabs.query(
+        {
+          active: true,
+          currentWindow: true
+        },
+        tabs => {
+          browser.tabs.sendMessage(
+            tabs[0].id,
+            {
+              message: 'get-text'
+            },
+            response => {
+              return resolve(response);
+            }
+          );
+        }
+      );
     });
   };
 
-  const copyToClipboard = async (url) => {
+  const sendMessageToPage = async (message, data = null) => {
+    browser.tabs.query(
+      {
+        active: true,
+        currentWindow: true
+      },
+      tabs => {
+        browser.tabs.sendMessage(tabs[0].id, {
+          message,
+          data
+        });
+      }
+    );
+  };
+
+  const copyToClipboard = async url => {
     try {
-      const {state} = await navigator.permissions.query({
-        name: 'clipboard-write',
+      const { state } = await navigator.permissions.query({
+        name: 'clipboard-write'
       });
       if (state !== 'granted') {
         throw new Error('Clipboard permission not granted');
@@ -118,5 +206,6 @@
       document.execCommand('copy');
       textArea.remove();
     }
+    await sendMessageToPage('success', url);
   };
 })(window.chrome || window.browser);
--- content_script.js
+++ content_script.js
@@ -1,22 +1,40 @@
-((browser) => {
+(browser => {
   if (!('fragmentDirective' in window.location)) {
     return;
   }
 
-  browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === 'get-text') {
+  const trimRemoveDuplicateSpaces = text => {
+    return text.trim().replace(/\s+/gm, ' ');
+  };
+
+  browser.runtime.onMessage.addListener((request, _, sendResponse) => {
+    const message = request.message;
+    if (message === 'get-text') {
       const {
         anchorNode,
         anchorOffset,
         focusNode,
-        focusOffset,
+        focusOffset
       } = window.getSelection();
       sendResponse({
-        pageText: document.body.textContent.trim().replace(/\s+/g, ' '),
+        pageText: trimRemoveDuplicateSpaces(document.body.textContent),
         textBeforeSelection: anchorNode.data.substr(0, anchorOffset).trim(),
         textAfterSelection: focusNode.data.substr(focusOffset).trim(),
+        textNodeBeforeSelection: focusNode.parentNode.previousElementSibling
+          ? trimRemoveDuplicateSpaces(
+              focusNode.parentNode.previousElementSibling.textContent
+            )
+          : '',
+        textNodeAfterSelection: anchorNode.parentNode.nextElementSibling
+          ? trimRemoveDuplicateSpaces(
+              anchorNode.parentNode.nextElementSibling.textContent
+            )
+          : ''
       });
+    } else if (message === 'success') {
+      console.log(request.data);
+    } else if (message === 'failure') {
+      alert(chrome.i18n.getMessage('link_failure'));
     }
   });
-
 })(window.chrome || window.browser);
