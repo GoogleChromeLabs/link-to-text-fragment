af62d525855da94d9bbc8b0bb3a751c290660d4d
herrkaefer <gloolar@gmail.com>
2022-03-24 10:14:27 -0500

    update var names & CSS styles #55
    according to Google style guides

--- options.js
+++ options.js
@@ -2,93 +2,91 @@
   document.querySelector('h1').textContent = browser.i18n.getMessage('link_copy_style');
   document.querySelector('.rich').textContent = browser.i18n.getMessage('rich');
   document.querySelector('.raw').textContent = browser.i18n.getMessage('raw');
-  document.querySelector('.rich_plus_raw').textContent = browser.i18n.getMessage('rich_plus_raw');
+  document.querySelector('.rich-plus-raw').textContent = browser.i18n.getMessage('rich_plus_raw');
   document.querySelector('h4').textContent = browser.i18n.getMessage('link_text');
-  document.querySelector('.link_text_option_1').textContent = browser.i18n.getMessage('link_text_option_1');
-  document.querySelector('.link_text_option_2').textContent = browser.i18n.getMessage('link_text_option_2');
-  document.querySelector('.link_text_option_3').textContent = browser.i18n.getMessage('link_text_option_3');
-  document.querySelector('.link_text_option_4').textContent = browser.i18n.getMessage('link_text_option_4');
+  document.querySelector('.link-text-option-1').textContent = browser.i18n.getMessage('link_text_option_1');
+  document.querySelector('.link-text-option-2').textContent = browser.i18n.getMessage('link_text_option_2');
+  document.querySelector('.link-text-option-3').textContent = browser.i18n.getMessage('link_text_option_3');
+  document.querySelector('.link-text-option-4').textContent = browser.i18n.getMessage('link_text_option_4');
 
   const rich = document.querySelector('#rich');
   const raw = document.querySelector('#raw');
-  const rich_plus_raw = document.querySelector('#rich_plus_raw');
+  const richPlusRaw = document.querySelector('#rich-plus-raw');
 
-  const link_text_option_1 = document.querySelector('#link_text_option_1');
-  const link_text_option_2 = document.querySelector('#link_text_option_2');
-  const link_text_option_3 = document.querySelector('#link_text_option_3');
-  const link_text_option_4 = document.querySelector('#link_text_option_4');
-  const link_text_input = document.querySelector('#link_text_input');
+  const linkTextOption1 = document.querySelector('#link-text-option-1');
+  const linkTextOption2 = document.querySelector('#link-text-option-2');
+  const linkTextOption3 = document.querySelector('#link-text-option-3');
+  const linkTextOption4 = document.querySelector('#link-text-option-4');
+  const linkTextInput = document.querySelector('#link-text-input');
 
   const enableLinkTextOptions = (enabled) => {
-    document.getElementsByName("link_text").forEach((e) => {
+    document.getElementsByName("link-text").forEach((e) => {
       e.disabled = !enabled;
     });
   };
 
   // Restore previous settings.
-  browser.storage.sync.get({linkStyle: 'rich', linkText: document.querySelector('.link_text_option_1').textContent}, (items) => {
+  browser.storage.sync.get({linkStyle: 'rich', linkText: document.querySelector('.link-text-option-1').textContent}, (items) => {
     rich.checked = items.linkStyle === 'rich';
     raw.checked = items.linkStyle === 'raw';
-    rich_plus_raw.checked = items.linkStyle === 'rich_plus_raw';
-
-    link_text_option_1.checked = items.linkText === document.querySelector('.link_text_option_1').textContent;
-    link_text_option_2.checked = items.linkText === document.querySelector('.link_text_option_2').textContent;
-    link_text_option_3.checked = items.linkText === document.querySelector('.link_text_option_3').textContent;
-    if (!link_text_option_1.checked && !link_text_option_2.checked && !link_text_option_3.checked) {
-        link_text_option_4.checked = true;
-        link_text_input.value = items.linkText;
+    richPlusRaw.checked = items.linkStyle === 'rich_plus_raw';
+    linkTextOption1.checked = items.linkText === document.querySelector('.link-text-option-1').textContent;
+    linkTextOption2.checked = items.linkText === document.querySelector('.link-text-option-2').textContent;
+    linkTextOption3.checked = items.linkText === document.querySelector('.link-text-option-3').textContent;
+    if (!linkTextOption1.checked && !linkTextOption2.checked && !linkTextOption3.checked) {
+        linkTextOption4.checked = true;
+        linkTextInput.value = items.linkText;
     }
-
-    enableLinkTextOptions(rich_plus_raw.checked);
+    enableLinkTextOptions(richPlusRaw.checked);
   });
 
   // Save settings on change.
   rich.addEventListener('click', () => {
     browser.storage.sync.set({linkStyle: 'rich'});
-    enableLinkTextOptions(rich_plus_raw.checked);
+    enableLinkTextOptions(richPlusRaw.checked);
   });
   raw.addEventListener('click', () => {
     browser.storage.sync.set({linkStyle: 'raw'});
-    enableLinkTextOptions(rich_plus_raw.checked);
+    enableLinkTextOptions(richPlusRaw.checked);
   });
-  rich_plus_raw.addEventListener('click', () => {
+  richPlusRaw.addEventListener('click', () => {
     browser.storage.sync.set({linkStyle: 'rich_plus_raw'});
-    enableLinkTextOptions(rich_plus_raw.checked);
+    enableLinkTextOptions(richPlusRaw.checked);
   });
 
-  link_text_option_1.addEventListener('click', () => {
+  linkTextOption1.addEventListener('click', () => {
     browser.storage.sync.set({
-      linkText: document.querySelector('.link_text_option_1').textContent,
+      linkText: document.querySelector('.link-text-option-1').textContent,
     });
   });
-  link_text_option_2.addEventListener('click', () => {
+  linkTextOption2.addEventListener('click', () => {
     browser.storage.sync.set({
-      linkText: document.querySelector('.link_text_option_2').textContent,
+      linkText: document.querySelector('.link-text-option-2').textContent,
     });
   });
-  link_text_option_3.addEventListener('click', () => {
+  linkTextOption3.addEventListener('click', () => {
     browser.storage.sync.set({
-      linkText: document.querySelector('.link_text_option_3').textContent,
+      linkText: document.querySelector('.link-text-option-3').textContent,
     });
   });
-  link_text_option_4.addEventListener('click', () => {
-    link_text_input.focus();
+  linkTextOption4.addEventListener('click', () => {
+    linkTextInput.focus();
     browser.storage.sync.set({
-      linkText: link_text_input.value,
+      linkText: linkTextInput.value,
     });
   });
-  link_text_input.addEventListener('focus', () => {
-    link_text_option_4.checked = true;
+  linkTextInput.addEventListener('focus', () => {
+    linkTextOption4.checked = true;
   });
-  link_text_input.addEventListener('change', () => {
-    if (link_text_input.value.replace(/\s/g, '').length > 0) {
+  linkTextInput.addEventListener('change', () => {
+    if (linkTextInput.value.replace(/\s/g, '').length > 0) {
       browser.storage.sync.set({
-          linkText: link_text_input.value,
+          linkText: linkTextInput.value,
       });
     }
     else {
       browser.storage.sync.set({
-        linkText: document.querySelector('.link_text_option_1').textContent,
+        linkText: document.querySelector('.link-text-option-1').textContent,
       });
     }
   });
